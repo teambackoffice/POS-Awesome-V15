fbbdcc9b88a3251fd719dcd6b53050b27f1c3e64
Sammish Thundiyil <sammish.thundiyil@gmail.com>
2025-07-13 00:10:58 +0530

    Update __init__.py

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
 
 __version__ = "15.2.2"
 
-
-def console(*data):
-    frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+try:
+    import frappe
+    
+    def console(*data):
+        frappe.publish_realtime("toconsole", data, user=frappe.session.user)
+except ImportError:
+    # frappe not available during build/installation
+    def console(*data):
+        pass
